e34f0dce4d4afff30960eb207b1d948e979d2106
Ryan MurMur <rianpmurphy104@gmail.com>
2026-04-23 18:52:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onresize = Anything
+window.onload = Anything
+
+function Anything()
+{
+    wwidth=window.innerWidth
+    document.getElementById("width").innerHTML=
+    "screenwidth:"+ wwidth+ " px"
+}
